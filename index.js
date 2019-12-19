956eacc201d4f764b8964d4996b584f2bc99a051
Jody Jones <jodybjones@yahoo.com>
2019-12-19 01:17:37 -0500

    adding axios and variables

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const axios = require("axios");
+//need to put github api address here
+axios
+  .get("https://www.omdbapi.com/?t=The%20Matrix&apikey=trilogy")
+  .then(function(res) {
+    console.log(res.data);
+  });
+
+  const fs = require('fs')
+  const inquire = require('inquirer')
+
 const questions = [
   
 ];
