a88fb4a84d32f0f762c67a1ecb1e63eece1d2635
Jody Jones <jodybjones@yahoo.com>
2019-12-19 01:42:15 -0500

    added questions

--- index.js
+++ index.js
@@ -8,10 +8,22 @@ axios
 
   const fs = require('fs')
   const inquire = require('inquirer')
-
+//need to reference these for later
 const questions = [
+{
+    type: "input",
+    message: "What is name on Github?",
+    name: "username"
+},
+{
+    type: "input",
+    message: "What is your favorite color?",
+    name: "color"
+}
   
 ];
+
+
 
 function writeToFile(fileName, data) {
  
